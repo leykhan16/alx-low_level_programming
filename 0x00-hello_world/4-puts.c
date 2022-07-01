0caa289a7aa40d47cf4bf08e73f29bace6383a1d
adebayo abiodun <106762233+leykhan16@users.noreply.github.com>
2022-07-01 19:28:36 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
-# include <stdio.h> 
+#include <stdio.h>
 /**
+ * main - print the strings that inputs the function
+ * Description:printing"Programming is like building a multilingual puzzle"
+ * Return:0
+ */
 int main(void)
- {
-puts("\"programming is like building a multilingual puzzle");
+{
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+ 
