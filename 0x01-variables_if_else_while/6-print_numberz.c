b04d6fdd32f30bb3b595b0b7c154e6975b1ddd2a
adebayo abiodun <106762233+leykhan16@users.noreply.github.com>
2022-07-01 21:15:51 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,19 @@
-task6
+#include <stdio.h>
+
+/**
+ * main - print single digit numbers
+ * Description: "prints all single digit numbers of base 10 starting from 0"
+ * Return: 0
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
