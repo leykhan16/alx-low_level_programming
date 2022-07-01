deeda78a6f2521a48ce5a9b8e26109d9c03c9b63
adebayo abiodun <106762233+leykhan16@users.noreply.github.com>
2022-07-01 21:23:24 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,1 +1,37 @@
-task100
+#include <stdio.h>
+
+/**
+ * main - prints combinations
+ * Description: prints all possible different combinations of two digits
+ * Return: 0
+ */
+int main(void)
+{
+	int a = '0';
+	int b = '9';
+
+	while (a <= '9')
+	{
+		while (b <= '9')
+		{
+			if (!(a > b || a == b))
+			{
+				putchar(a);
+				putchar(b);
+				if (a == '8' && b == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			b++;
+		}
+		b = '0';
+		a++;
+	}
+	return (0);
+}
