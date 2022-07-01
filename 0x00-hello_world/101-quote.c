34b5ed4945ebb9ffc3f19fc813d4ab3472516d30
adebayo abiodun <106762233+leykhan16@users.noreply.github.com>
2022-07-01 19:51:31 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,13 @@
-task8
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - use return value 1
+ * Description: prints the standard output in the main function
+ * Return: 1
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
