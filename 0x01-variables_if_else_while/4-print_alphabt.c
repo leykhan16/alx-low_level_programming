92d89d5580007b47a3df8ac75a1c2c5a26d89689
adebayo abiodun <106762233+leykhan16@users.noreply.github.com>
2022-07-01 21:12:28 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,21 @@
-task4
+#include <stdio.h>
+/**
+ * main - printing alphabets in lowercase
+ * Description: program that prints the alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
