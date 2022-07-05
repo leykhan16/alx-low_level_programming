a95cf74740f28d66a6ef5bafcc202cbb39bf2496
adebayo abiodun <106762233+leykhan16@users.noreply.github.com>
2022-07-05 21:21:33 +0100

    Update positive_or_negative.c

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,1 +1,34 @@
-task0
+#include <stdio.h>
+
+/**
+ * main - prints integer as positive or negative
+ * Description: prints as positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
+
+
+/**
+ * positive_or_negative - prints numbers negative or positive
+ * Description: prints positive or negative
+ * Return: void
+ */
+
+void positive_or_negative(int i)
+
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	} else if (i < 0)
+		printf("%d is negative\n", i);
+}
