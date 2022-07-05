dead094ce2440fef27cadaa6e750fdd048544459
adebayo abiodun <106762233+leykhan16@users.noreply.github.com>
2022-07-05 08:12:42 +0100

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,14 @@
-task6
+#include "main.h"
+
+/**
+ * _abs - calculate absolute value
+ * Description: calculate the absolute value of a number from zero.
+ * @n: the number to be converted.
+ * Return: absolute value of the input number.
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = n * -1;
+	return (n);
+}
