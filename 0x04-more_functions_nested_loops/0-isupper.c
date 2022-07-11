22332fba8a666b55b4a99531d1876ea2cf989acb
adebayo abiodun <106762233+leykhan16@users.noreply.github.com>
2022-07-11 09:14:46 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,17 @@
-task0
+#include "holberton.h"
+
+/**
+ * _isupper - checks for upper case of a char
+ * @c: char to be cheked
+ *
+ *
+ *
+ * Return: 1 if upper, 0 if not
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
