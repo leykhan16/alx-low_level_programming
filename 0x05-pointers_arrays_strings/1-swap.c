737d8cc7733b73a6e4eac582655326251cae190c
adebayo abiodun <106762233+leykhan16@users.noreply.github.com>
2022-07-12 09:50:32 +0100

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,16 @@
-task1
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - switches the value of first input with the input of the second
+ * @a: the first integer
+ * @b: the second integer
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
