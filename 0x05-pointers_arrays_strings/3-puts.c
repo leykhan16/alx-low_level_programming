5ec6414d26ad3c6f34c81f88df902d0b95c3f4b7
adebayo abiodun <106762233+leykhan16@users.noreply.github.com>
2022-07-12 09:52:20 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,16 @@
-task3
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: input string
+ * Return: 0
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
