b449e190d44bec71925ec34a5aff535809631f11
adebayo abiodun <106762233+leykhan16@users.noreply.github.com>
2022-07-22 16:40:00 +0100

    Update main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,5 @@
-#ifndef FILE_HOLBERTON
-#define FILE_HOLBERTON
-
+#ifndef HEADERFILE_H
+#define HEADERFILE_H
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -21,5 +20,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
 #endif
